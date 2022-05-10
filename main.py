bfcb35578ff2142e0df30160803b1bf44d9ab3c0
Andrew Lim <andrewjcl88@gmail.com>
2022-05-10 14:40:03 +1000

    score and time

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from operator import truediv
 import pygame
 from sys import exit
 
+def display_score():
+    current_time = pygame.time.get_ticks()
+    score_surf = test_font.render(current_time, False, (64, 64, 64))
+    score_rect = score_surf.get_rect(center = (400, 50))
+    screen.blit(score_surf, score_rect)
+
+
+
+
 """ Iniatialise """
 pygame.init()                                       # Initialises pygame systems
 screen = pygame.display.set_mode((800, 400))        # Sets main window
@@ -12,9 +21,6 @@ game_active = True
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-
-score_surf = test_font.render('My game', False, (80, 80, 80))
-score_rect = score_surf.get_rect(center = (400, 50))
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(midbottom = (835, 300))
@@ -31,19 +37,33 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if player_rect.collidepoint(event.pos) and player_rect.bottom == 300:
-                player_gravity = -12
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and player_rect.bottom == 300:
-                player_gravity = -12
+
+        # Game screen
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rect.collidepoint(event.pos) and player_rect.bottom == 300:
+                    player_gravity = -12
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.bottom == 300:
+                    player_gravity = -12
+        # Menu screen
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                snail_rect.right = 800
+                player_rect.bottom = 300
+                player_gravity = 0
+                game_active = True
+
+
 
     if game_active:
         screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, 300))
-        pygame.draw.rect(screen, '#c0e8ec', score_rect)
-        pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
-        screen.blit(score_surf, score_rect)
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect)
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+        # screen.blit(score_surf, score_rect)
+        display_score()
+
 
         # Snail
         snail_rect.x -= 4
@@ -70,8 +90,7 @@ while True:
 
     pygame.display.update()
     clock.tick(60)
-    print(player_gravity)
-
-
-    # 1:53:00
+    
+    # 2:00:28
+    # Why does ticks() only count active time? Can I count menu time?
     # https://www.youtube.com/watch?v=AY9MnQ4x3zk
